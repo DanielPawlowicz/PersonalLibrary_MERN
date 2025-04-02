9badbcb50dee1ca32a31a6231aa61a1351755e0c
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2025-04-02 20:42:04 +0200

    bookModel - mongoose Schema

---
+++ backend/models/bookModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    cover: {
+        type: String,
+        required: false
+    },
+    format: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    review: {
+        type: String,
+        required: false
+    },
+    notes: {
+        type: [String],
+        required: false
+    },
+    isOwned: {
+        type: Boolean,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    order: {
+        type: Number,
+        required: true
+    },
+    link: {
+        type: String,
+        required: false
+    }
+})
+
+module.exports = mongoose.model('Book', bookSchema)
