efce10210e3991f6e44ad25a6a695278c4eb0d0c
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2025-04-02 20:44:57 +0200

    books routes - finished

---
+++ backend/routes/books.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const {
+    getBooks,
+    getBook,
+    createBook,
+    deleteBook,
+    updateBook
+} = require('../controllers/bookController')
+
+const router = express.Router()
+
+// GET all books
+router.get('/', getBooks)
+
+// GET a single book
+router.get('/:id', getBook)
+
+// POST a new book
+router.post('/', createBook)
+
+// DELETE a book
+router.delete('/:id', deleteBook)
+
+// UPDATE a book
+router.patch('/:id', updateBook)
+
+module.exports = router
