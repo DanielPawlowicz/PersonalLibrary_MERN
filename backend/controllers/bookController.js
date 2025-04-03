3bd7450b704fb560f8b24512c84b50b506ed5dea
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2025-04-03 16:11:52 +0200

    update book - controller

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -66,7 +66,21 @@ const deleteBook = async (req, res) => {
 
 // update a Book
 const updateBook = async (req, res) => {
-    res.json({ mssg: 'update' })
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such book' })
+    }
+
+    const book = await Book.findOneAndUpdate({ _id: id }, {
+        ...req.body
+    })
+
+    if (!book) {
+        return res.status(400).json({ error: 'No such book' })
+    }
+
+    res.status(200).json(book)
 }
 
 module.exports = {
