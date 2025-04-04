7e329e8db1ae9cf0809f988301364fec93a7a931
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2025-04-04 15:41:13 +0200

    BookFullDisplay mechanism

--- frontend/src/components/BookDetails.js
+++ frontend/src/components/BookDetails.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useBooksContext } from '../hooks/useBooksContext'
+import BookFullDisplay from '../pages/BookFullDisplay';
 
 const BookDetails = ({ book }) => {
 
     const { dispatch } = useBooksContext()
+    const [isDisplayed, setIsDisplayed] = useState(false);
 
     return (
         <div className='book-details'>
             <h4>{book.title}</h4>
+            <button onClick={() => { setIsDisplayed(true) }}>More...</button>
+            {
+                book && isDisplayed && <BookFullDisplay book={book} />
+            }
         </div>
     )
 }
---
+++ frontend/src/pages/BookFullDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BookFullDisplay = ({ book }) => {
+    return (
+        <div>BookFullDisplay
+            <h2>{book.title}</h2>
+        </div>
+    )
+}
+
+export default BookFullDisplay
