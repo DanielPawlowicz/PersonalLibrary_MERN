51763356cdfef52eb8e138e9a5bdf07eb98f3bab
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2025-04-04 14:58:17 +0200

    fetching all books in bookshelf

--- frontend/src/pages/Bookshelf.js
+++ frontend/src/pages/Bookshelf.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import { useEffect } from 'react'
+import BookDetails from '../components/BookDetails'
+import { useBooksContext } from '../hooks/useBooksContext'
 
 const Bookshelf = () => {
+
+    const { books, dispatch } = useBooksContext()
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const response = await fetch('/api/books')
+            const json = await response.json()
+
+            if (response.ok) {
+                dispatch({ type: 'SET_BOOKS', payload: json })
+            }
+        }
+
+        fetchBooks()
+    }, [dispatch])
+
     return (
-        <div>Bookshelf</div>
+        <div className='home'>
+            <div className='books'>
+                {books && books.map((book) => (
+                    <BookDetails key={book._id} book={book} />
+                ))}
+            </div>
+        </div>
     )
 }
 
