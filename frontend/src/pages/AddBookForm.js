1d7e7ce59ae7ce01d3f68d266ab0d800766f9fd7
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2025-04-07 16:59:27 +0200

    Add Book Form - working in console

--- frontend/src/pages/AddBookForm.js
+++ frontend/src/pages/AddBookForm.js
@@ -36,6 +36,98 @@ const AddBookForm = () => {
             <form className='create-form' onSubmit={handleSubmit}>
                 <h3>Add a New Book</h3>
 
+                <label>Title:</label>
+                <input
+                    type="text"
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                    className={emptyFields.includes('title') ? 'error' : ''}
+                />
+
+                <label>Author:</label>
+                <input
+                    type="text"
+                    onChange={(e) => setAuthor(e.target.value)}
+                    value={author}
+                    className={emptyFields.includes('author') ? 'error' : ''}
+                />
+
+                <label>Cover:</label>
+                <input
+                    type="text"
+                    onChange={(e) => setCover(e.target.value)}
+                    value={cover}
+                />
+
+                <label>Format:</label>
+                <select
+                    id="format"
+                    onChange={(e) => setFormat(e.target.value)}
+                    value={format}
+                    className={emptyFields.includes('title') ? 'error' : ''}
+                >
+                    <option value="Paperback">Paperback</option>
+                    <option value="Ebook Reader">Ebook Reader</option>
+                    <option value="PDF">PDF</option>
+                    <option value="Other">Other</option>
+                </select>
+
+                <label>Tags:</label>
+                <span>* Type in tags separated with a coma and a space, e.g.: fantasy, classic, war</span>
+                <input
+                    type="text"
+                    onChange={(e) => setTags(e.target.value)}
+                    value={tags}
+                />
+
+                <label>Description:</label>
+                <input
+                    type="text"
+                    onChange={(e) => setDescription(e.target.value)}
+                    value={description}
+                />
+
+                <label>Review:</label>
+                <input
+                    type="text"
+                    onChange={(e) => setReview(e.target.value)}
+                    value={review}
+                />
+
+                <label>Do you already have this book?</label>
+                <label>
+                    <input
+                        type="radio"
+                        name="ownsBook"
+                        value="true"
+                        checked={isOwned === 'true'}
+                        onChange={(e) => setIsOwned(e.target.value)}
+                        className={emptyFields.includes('isOwned') ? 'error' : ''}
+                    />
+                    Yes, I already have this book
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        name="ownsBook"
+                        value="false"
+                        checked={isOwned === 'false'}
+                        onChange={(e) => setIsOwned(e.target.value)}
+                        className={emptyFields.includes('isOwned') ? 'error' : ''}
+                    />
+                    No, but I wish to have this book
+                </label>
+
+                <label>Link:</label>
+                <span>* you can add a link related with the book: store, review site etc.</span>
+                <input
+                    type="text"
+                    onChange={(e) => setLink(e.target.value)}
+                    value={link}
+                />
+
+                <button>Add the Book</button>
+                {error && <div className='error'>{error}</div>}
 
             </form>
         </div>
