dc9c7b45e695c01ebb48f64173e053cc70bf690e
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2025-04-08 21:23:05 +0200

    Book Full Display working

--- frontend/src/pages/BookFullDisplay.js
+++ frontend/src/pages/BookFullDisplay.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 const BookFullDisplay = ({ book, setIsDisplayed }) => {
-    return (
-        <div className='book-full'>BookFullDisplay
-            <h2>{book.title}</h2>
-        </div>
+    return ReactDOM.createPortal(
+        <>
+            <div className='overlay' onClick={() => setIsDisplayed(false)}></div>
+            <div className='book-full'>
+                <button onClick={() => setIsDisplayed(false)}>Close</button>
+                <h2>{book.title}</h2>
+                <p>Author: {book.author}</p>
+                {/* more book details here */}
+            </div>
+        </>,
+        document.body // render it outside the component tree
     )
 }
 
