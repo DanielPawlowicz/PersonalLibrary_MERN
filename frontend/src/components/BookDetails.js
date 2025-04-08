bd6b3faf61648ca474f8cc7e9ef079b244b82c64
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2025-04-08 21:09:50 +0200

    Format icons

--- frontend/src/components/BookDetails.js
+++ frontend/src/components/BookDetails.js
@@ -1,11 +1,33 @@
 import React, { useState } from 'react'
 import { useBooksContext } from '../hooks/useBooksContext'
 import BookFullDisplay from '../pages/BookFullDisplay';
+import { TbPdf } from "react-icons/tb"; // pdf
+import { MdOutlineDeviceUnknown } from "react-icons/md"; // other
+import { GiBlackBook } from "react-icons/gi"; // book
+import { MdOutlineTabletMac } from "react-icons/md"; // ebook reader
+
 
 const BookDetails = ({ book }) => {
 
     const { dispatch } = useBooksContext()
     const [isDisplayed, setIsDisplayed] = useState(false);
+
+    // Format icon logic
+    const getFormatIcon = (format) => {
+        switch (format) {
+            case 'Paperback':
+                return <GiBlackBook />
+            case 'Ebook Reader':
+                return <MdOutlineTabletMac />
+            case 'PDF':
+                return <TbPdf />
+            case 'Other':
+                return <MdOutlineDeviceUnknown />
+            default:
+                return null
+        }
+    }
+
 
     return (
         <div className='book-details'>
@@ -14,7 +36,12 @@ const BookDetails = ({ book }) => {
                     ? <img src={book.cover} className='book-cover' alt={`${book.title} cover`} />
                     : <div className='alternate-cover'><h3>{book.title}</h3></div>
             }
-            <h4>{book.title}</h4>
+
+            <div className='book-details-icon'>
+                {getFormatIcon(book.format)}
+            </div>
+
+            <h4 className='book-details-title'>{book.title}</h4>
             <span className='book-details-author'>{book.author}</span><br />
             <button onClick={() => { setIsDisplayed(true) }}>More...</button>
             {
