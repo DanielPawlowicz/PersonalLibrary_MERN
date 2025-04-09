0c7ebdbaffad4d33a43c78c2349ceedbaa66f468
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2025-04-09 20:38:16 +0200

    Better display

--- frontend/src/pages/BookFullDisplay.js
+++ frontend/src/pages/BookFullDisplay.js
@@ -11,20 +11,25 @@ const BookFullDisplay = ({ book, setIsDisplayed }) => {
             <div className='overlay' onClick={() => setIsDisplayed(false)}></div>
             <div className='book-full'>
                 <IoMdClose className="close-icon" onClick={() => setIsDisplayed(false)} />
-                <h2>{book.title}</h2>
-                <p className='book-full-info'><span className='book-full-label'>Author: </span>{book.author}</p>
-                <p className='book-full-info'><span className='book-full-label'>Format: </span>{book.format}</p>
-                <ul>
+                <div className='book-full-left'>
+                    <h2>{book.title}</h2>
+                    <p className='book-full-info'><span className='book-full-label'>Author: </span>{book.author}</p>
+                    <p className='book-full-info'><span className='book-full-label'>Format: </span>{book.format}</p>
+                    <ul>
+                        {
+                            book.tags?.map((tag, i) => (
+                                <li key={i}>{tag}</li>
+                            ))
+                        }
+                    </ul>
+                    {/* more book details here */}
+                </div>
+                <div className='book-full-right'>
                     {
-                        book.tags?.map((tag, i) => (
-                            <li key={i}>{tag}</li>
-                        ))
+                        book.cover && <img src={book.cover} className='book-cover-full' alt={`${book.title} cover`} />
                     }
-                </ul>
-                {
-                    book.cover && <img src={book.cover} className='book-cover-full' alt={`${book.title} cover`} />
-                }
-                {/* more book details here */}
+                </div>
+                <div className='clear'></div>
             </div>
         </>,
         document.body // render it outside the component tree
