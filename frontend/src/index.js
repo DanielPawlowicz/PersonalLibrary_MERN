bc3d5b1f0e5fd2676cd953f83fcd6f30b89179b7
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2025-04-17 11:33:32 +0200

    Prepare to deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import './index.css';
 import { BooksContextProvider } from './context/BooksContext';
 import { AuthContextProvider } from './context/AuthContext';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
