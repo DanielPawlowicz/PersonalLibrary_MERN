ef9f335e456211bb4f1c6258e39d9e73052cbf2b
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2025-06-26 14:38:21 +0200

    Password visibility toggle in login and sin up forms

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
 import { useLogin } from '../hooks/useLogin'
+import { FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
 
     const [email, setEmail] = useState('')
     const [password, setpassword] = useState('')
     const { login, error, isLoading } = useLogin()
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(prev => !prev);
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -27,11 +33,14 @@ const Login = () => {
 
             <label>Password: </label>
             <input
-                type="password"
+                type={passwordVisible ? 'text' : 'password'}
                 onChange={(e) => { setpassword(e.target.value) }}
                 value={password}
+                className="passwordInput"
             />
+            <FaEyeSlash className='togglePassword' onClick={togglePasswordVisibility} />
             {/* <button>Log in</button> */}
+            <br />
             <button disabled={isLoading}>Log in</button>
             {error && <div className='error'>{error}</div>}
         </form>
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
 import { useSignup } from '../hooks/useSignup'
+import { FaEyeSlash } from "react-icons/fa";
 
 const Signup = () => {
 
     const [email, setEmail] = useState('')
     const [password, setpassword] = useState('')
     const { signup, error, isLoading } = useSignup()
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(prev => !prev);
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -27,11 +33,14 @@ const Signup = () => {
 
             <label>Password: </label>
             <input
-                type="password"
+                type={passwordVisible ? 'text' : 'password'}
                 onChange={(e) => { setpassword(e.target.value) }}
                 value={password}
+                className="passwordInput"
             />
-            {/* <button>Sign Up</button> */}
+            <FaEyeSlash className='togglePassword' onClick={togglePasswordVisibility} />
+            {/* <button>Log in</button> */}
+            <br />
             <button disabled={isLoading}>Sign Up</button>
             {error && <div className='error'>{error}</div>}
         </form>
